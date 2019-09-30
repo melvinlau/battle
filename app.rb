4780a795520f5e836c1b2a46427f1752fb4be3ff
Melvin Lau <lau.melvin@icloud.com>
2019-09-30 18:21:00 +0100

    Added POST-redirect

--- app.rb
+++ app.rb
@@ -1,18 +1,29 @@
 require "sinatra/base"
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get "/" do
     erb :index
   end
 
+  # Use the POST method to store params in the session hash
+  # then redirect to /play
   post "/names" do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect "/play"
+  end
+
+  # Use the GET method to store session data in instance variables
+  get "/play" do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
+
+
   #start server if ruby file executed directly
   run! if app_file == $0
-
-
 end
